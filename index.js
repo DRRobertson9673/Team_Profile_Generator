c4f9c8a22954e7a7302c5b7a5f195043d6a9a47a
DRRobertson9673 <drrobertson9673@gmail.com>
2023-02-24 19:35:10 +0000

    refactored code to make it flow in a more sequential order

--- index.js
+++ index.js
@@ -10,145 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-function createEngineer(team) {
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "What is the engineer's name?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add a name';
-            }
-        },
-        {
-            name: "id",
-            message: "What is the engineer's ID?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add an ID';
-            }
-        },
-        {
-            name: "email",
-            message: "What is the engineer's email?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add an email';
-            }
-        },
-        {
-            name: "github",
-            message: "What is the engineer's GitHub username?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add a GitHub username';
-            }
-        },
-    ]).then((engineerDetails) => {
-        const engineer = new Engineer(engineerDetails.name, engineerDetails.id, engineerDetails.email, engineerDetails.github);
-        team.push(engineer);
-        createTeam(team); // at this point we add an engineer to the team array
-    });
-}
-
-function createIntern(team) {
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "What is the intern's name?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add a name';
-            }
-        },
-        {
-            name: "id",
-            message: "What is the intern's ID?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add an ID';
-            }
-        },
-        {
-            name: "email",
-            message: "What is the intern's email address?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add an email address';
-            }
-        },
-        {
-            name: "name",
-            message: "What is the intern's school?",
-            type: "text",
-            validate(value) {
-                if (value.length) {
-                    return true;
-                }
-                return 'You must add a school';
-            }
-        },
-    ]).then((internDetails) => {
-        // Initialise Intern class to create Manager object
-        const intern = new Intern(internDetails.name, internDetails.id, internDetails.email, internDetails.school)
-        team.push(intern);
-        createTeam(team); // at this point we add an intern to the team array
-    });
-}
-
-function createTeam(team) {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'memberChoice',
-            message: 'Which type of team member you wan to add?',
-            choices: [
-                'Engineer',
-                'Intern',
-                "I don't want to add any more team member",
-            ],
-        }
-    ]).then((choice) => {
-        if (choice.memberChoice === 'Engineer') {
-            createEngineer(team);
-        } else if (choice.memberChoice === 'Intern') {
-            createIntern(team);
-        } else {
-            console.log('made it this far');
-            // at this point, team array should have a manager and however many engineers and interns the user inputted
-            const html = render(team); // html will be html file as string
-            // write html to a file index.html using fs library
-            fs.writeFile('./index.html', html, (err) => {
-                if (err) {
-                    console.log('Failed to write HTML file');
-                }
-            });
-        }
-    });
-}
-
+// code to gather information on and create manager
 function createManager(team) {
     inquirer.prompt([
         {
@@ -203,6 +65,148 @@ function createManager(team) {
     });
 }
 
+// code to gather information on and create engineer
+function createEngineer(team) {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the engineer's name?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add a name';
+            }
+        },
+        {
+            name: "id",
+            message: "What is the engineer's ID?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add an ID';
+            }
+        },
+        {
+            name: "email",
+            message: "What is the engineer's email?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add an email';
+            }
+        },
+        {
+            name: "github",
+            message: "What is the engineer's GitHub username?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add a GitHub username';
+            }
+        },
+    ]).then((engineerDetails) => {
+        // Initialise Engineer class to create engineer object
+        const engineer = new Engineer(engineerDetails.name, engineerDetails.id, engineerDetails.email, engineerDetails.github);
+        team.push(engineer);
+        createTeam(team); // at this point we add an engineer to the team array
+    });
+}
+
+// code to gather info on and create intern
+function createIntern(team) {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the intern's name?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add a name';
+            }
+        },
+        {
+            name: "id",
+            message: "What is the intern's ID?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add an ID';
+            }
+        },
+        {
+            name: "email",
+            message: "What is the intern's email address?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add an email address';
+            }
+        },
+        {
+            name: "name",
+            message: "What is the intern's school?",
+            type: "text",
+            validate(value) {
+                if (value.length) {
+                    return true;
+                }
+                return 'You must add a school';
+            }
+        },
+    ]).then((internDetails) => {
+        // Initialise Intern class to create intern object
+        const intern = new Intern(internDetails.name, internDetails.id, internDetails.email, internDetails.school)
+        team.push(intern);
+        createTeam(team); // at this point we add an intern to the team array
+    });
+}
+
+// code to add additional members to team
+function createTeam(team) {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberChoice',
+            message: 'Which type of team member you wan to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                "I don't want to add any more team member",
+            ],
+        }
+    ]).then((choice) => {
+        if (choice.memberChoice === 'Engineer') {
+            createEngineer(team);
+        } else if (choice.memberChoice === 'Intern') {
+            createIntern(team);
+        } else {
+            // at this point, team array should have a manager and however many engineers and interns the user inputted
+            const html = render(team); // html will be html file as string
+            // write html to a file index.html using fs library
+            fs.writeFile('./index.html', html, (err) => {
+                if (err) {
+                    console.log('Failed to write HTML file');
+                }
+            });
+        }
+    });
+}
+
+// code to begin creating a new team
 function start() {
     const team = []; // array of Employee objects (array of Manager, or Engineers, or Interns)
     // Employee can be Manager, Engineer, or Intern
